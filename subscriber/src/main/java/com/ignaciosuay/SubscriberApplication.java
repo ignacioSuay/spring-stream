10cb4406ddd66e53c14a92a3295910e47b2770b1
ignacio suay <ignacio.suay@gmail.com>
2017-02-26 19:31:14 +0000

    remove enablebindings from main class

--- subscriber/src/main/java/com/ignaciosuay/SubscriberApplication.java
+++ subscriber/src/main/java/com/ignaciosuay/SubscriberApplication.java
@@ -1,11 +1,8 @@
 package com.ignaciosuay;
 
-import com.ignaciosuay.message.InputChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 
-@EnableBinding(InputChannel.class)
 @SpringBootApplication
 public class SubscriberApplication {
 
