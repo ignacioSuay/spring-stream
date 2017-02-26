31280b1e521c98d17a45dc6f50358fddb6ae083c
ignacio suay <ignacio.suay@gmail.com>
2017-02-26 19:30:53 +0000

    improve publisher rest

--- publisher/src/main/java/com/ignaciosuay/resource/MessageResource.java
+++ publisher/src/main/java/com/ignaciosuay/resource/MessageResource.java
@@ -1,9 +1,9 @@
 package com.ignaciosuay.resource;
 
+import com.ignaciosuay.message.OutputChannel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MessageResource {
 
-    private final MessageChannel output;
+    private final OutputChannel channel;
 
-    @RequestMapping(method = RequestMethod.GET, value= "/sendMessage/{message}" )
-    public void sendMessage(@PathVariable String message){
-        log.info("Receive message {}",message);
+    @RequestMapping(method = RequestMethod.GET, value = "/sendMessage/{message}")
+    public String sendMessage(@PathVariable String message) {
+        log.info("Receive message {}", message);
         Message<String> msg = MessageBuilder.withPayload(message).build();
-        output.send(msg);
-        log.info("Message sent to the publishers");
+        channel.output().send(msg);
+        return "Message " + message + " sent to the publishers";
     }
 }
